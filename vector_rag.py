a87b90ac11597af56697e4ce5881fddb503d8353
Kapil <kydv901@gmail.com>
2025-06-26 19:30:34 +0530

    updated the depreciated packages

--- vector_rag.py
+++ vector_rag.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAI
