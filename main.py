0807ac94900568bf5efe1b79dc6112e2a576d46a
Kapil <kydv901@gmail.com>
2025-10-15 19:49:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,9 @@ app = FastAPI()
 
 # Serve static files (CSS, JS)
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Serve HTML templates
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-# Include your API endpoints
 app.include_router(router)
